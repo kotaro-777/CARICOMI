423402e4c14704a5fe92bc403025a085addfc60d
kotaro-777 <kou.aaa36@icloud.com>
2021-01-19 13:03:33 +0900

    一旦コミット

--- venv_private_diary/private_diary/reviews/models.py
+++ venv_private_diary/private_diary/reviews/models.py
@@ -1,32 +1,34 @@
 from django.db import models
-#accountsアプリに作成したCustomUserをインポート
+# accountsアプリに作成したCustomUserをインポート
 from accounts.models import CustomUser
 
 
 # Create your models here.
 
-#Service
+# Service
 
 
-#口コミモデル
+# 口コミモデル
 class Reviews(models.Model):
-    user = models.ForeignKey(CustomUser, verbose_name="ユーザー", on_delete=models.PROTECT)
+    user = models.ForeignKey(
+        CustomUser, verbose_name="ユーザー", on_delete=models.PROTECT)
     service_name = models.CharField(verbose_name="サービス名", max_length=20)
     SERVICE_QUALITY_CHOICES = [
-        # (データベースに登録されている値, 表示上の値) ※表示上の値はget_field名_displayで取得
+        # (データベースに登録されている値, 表示上の値) ※表示上の値はget_フィールド名_displayで取得
         ('5', '★★★★★'),
         ('4', '★★★★'),
         ('3', '★★★'),
         ('2', '★★'),
         ('1', '★')
     ]
-    support_quality = models.CharField(verbose_name="サポートの質", choices=SERVICE_QUALITY_CHOICES, max_length=5)
+    support_quality = models.CharField(
+        verbose_name="サポートの質", choices=SERVICE_QUALITY_CHOICES, max_length=5)
     subject = models.TextField(verbose_name="サービスの感想")
     created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
     update_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
-    
+
     class Meta:
         verbose_name_plural = 'Reviews'
-    
+
     def __str__(self):
         return self.service_name
